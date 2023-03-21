09e9939248070c202881f482edd8aca33ea74b6d
vesnetskaya <lisa72117@gmail.com>
2023-03-22 00:02:17 +0300

    Метод выполнения задания

--- Программа/store/tasks.py
+++ Программа/store/tasks.py
@@ -2,3 +2,7 @@ class Tasks():
     def __init__(self):
         self.tasks = []
         self.done_tasks = []
+
+    def done(self, task):
+        self.tasks.remove(task)
+        self.done_tasks.add(task)
